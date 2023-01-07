71a3bfcec9e12a335fd6439c32269d75ca6ed4d1
CalledSidd <sidharthsidhu12317@gmail.com>
2023-01-07 15:48:30 +0530

    changed model field to imagekit photo model, removed migrations manually from db, changed views to add posts

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from accounts.models import UserAccount
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFit
 import uuid
 
 # Create your models here.
@@ -10,9 +12,9 @@ def posts(instance, filename):
 
 
 class Post(models.Model):
-    id      = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    id      = models.UUIDField(primary_key=True,editable=False, default=uuid.uuid4)
     user    = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
-    image   = models.ImageField(upload_to= posts, null=True)
+    image   = ProcessedImageField(upload_to= posts, format = "JPEG", options={"quality":90}, processors=[ResizeToFit(width=1200, height=1200)])
     title   = models.TextField(max_length=80)
     likes   = models.IntegerField(default=0)
     reports = models.IntegerField(default=0)
